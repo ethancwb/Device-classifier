bbff898a0ad39e3235d16c976e14f297ee9f1a0c
ykottu <kottu.y@husky.neu.edu>
2018-02-14 21:13:43 -0500

    Includes both DNS and HTPP features consolidated

--- dns_data_processor.py
+++ dns_data_processor.py
@@ -4,11 +4,15 @@ import numpy as np
 from tabulate import tabulate
 
 path = 'E:\\MonIOTr\\0%3Ae%3Af3%3A3b%3A85%3Ae5\\0-e-f3-3b-85-e5-dns.csv'
+path__http = 'E:\\MonIOTr\\0%3Ae%3Af3%3A3b%3A85%3Ae5\\0-e-f3-3b-85-e5-http.csv'
+
 df = pd.read_csv(path,error_bad_lines=False,sep='\t')
+df_http = pd.read_csv(path__http,error_bad_lines=False,sep='\t')
 
 class data_feature_extractor:
-    def __init__(self, df):
+    def __init__(self, df,df_http):
         self.dataframe = df
+        self.dataframe_http=df_http
         self.features = {}
         self.formatted = {
             "feature_strings": {},
@@ -91,6 +95,57 @@ class data_feature_extractor:
             counts[name]=temp.count(name)
         for name in counts:
             self.formatted['feature_strings'][name] = float(counts[name] / self.totalPackets)
+
+    def content_length(self):
+        print(self.dataframe_http)
+        conLen = self.dataframe_http["http.content_length"]
+        counts = conLen.value_counts()
+        name_list = counts.index.tolist()
+        for each in conLen.unique():
+            if each != 'http.content_length':
+                self.features[each] = 1
+        for name in name_list:
+            if name != 'http.content_length':
+                self.formatted['others'].append(int(name))
+
+    def content_type(self):
+        conType = self.dataframe_http["http.content_type"]
+        counts = conType.value_counts()
+        name_list = counts.index.tolist()
+        for each in conType.unique():
+            if each != 'http.content_type':
+                self.features[each] = 1
+        for name in name_list:
+            if name != 'http.content_type':
+                self.formatted['others'].append(name)
+    def feature_http_host(self):
+        host = self.dataframe_http["http.host"]
+        counts = host.value_counts()
+        name_list = counts.index.tolist()
+        for name in name_list:
+            if name != "http.host":
+                self.formatted["feature_strings"][name.split("_")[0]] = float(counts[name] / self.totalPackets)
+    def feature_http_request_uri(self):
+        uri = self.dataframe_http["http.request.uri"]
+        counts = uri.value_counts()
+        name_list = counts.index.tolist()
+        for name in name_list:
+            if name != "http.request.uri":
+                self.formatted["feature_strings"][name.split("_")[0]] = float(counts[name] / self.totalPackets)
+    def feature_http_server(self):
+        server = self.dataframe_http["http.server"]
+        counts = server.value_counts()
+        name_list = counts.index.tolist()
+        for name in name_list:
+            if name != "http.server":
+                self.formatted["feature_strings"][name.split("_")[0]] = float(counts[name] / self.totalPackets)
+    def feature_http_response(self):
+        res = self.dataframe_http["http.response.line"]
+        counts = res.value_counts()
+        name_list = counts.index.tolist()
+        for name in name_list:
+            if name != "http.response.line":
+                self.formatted['feature_strings'][name]=float(counts[name] / self.totalPackets)
     def formatted_output(self):
         self.feature_eth_src()
         self.pckt_length()
@@ -98,9 +153,18 @@ class data_feature_extractor:
         self.cname()
         self.qname()
         self.rname()
+        self.content_length()
+        self.content_type()
+
+        self.feature_http_host()
+        self.feature_http_request_uri()
+        self.feature_http_response()
+        self.feature_http_server()
         totalCount = len(self.dataframe)
         self.formatted['total_packets'] = totalCount
         print (self.formatted)
         return self.formatted
-fe_obj = data_feature_extractor(df)
+
+
+fe_obj = data_feature_extractor(df,df_http)
 fe_obj.formatted_output()
